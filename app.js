87f47bc8667eb8ec9161f67aab8b177197b49120
cristiansandu22 <cristian.sandu@welthee.com>
2023-10-30 16:56:11 +0200

    Vue lifecycle hooks
    Tested Vue lifecycle hooks.

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
+      message: "Hello world!",
+      anotherMessage: "Noooo"
     }
-  }
+  },
+
+  beforeCreate() {
+    console.log('beforeCreated() function called!', this.message)
+  },
+
+  created() {
+    console.log('created() function called!', this.message)
+  },
+
+  beforeMount() {
+    console.log('beforeMount() function called!', this.$el, this.message);
+  },
+
+  mounted() {
+    console.log('mounted() function called!', this.$el, this.message);
+  },
 })
 
 vm.mount('#app');
